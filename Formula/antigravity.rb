5fe64c855530d11ac434d2f616a6d67fa02958fa
BALAJI S <212744006+Balajitechlabs@users.noreply.github.com>
2026-05-22 17:28:05 +0530

    Initial commit of Homebrew Tap

---
+++ Formula/antigravity.rb
@@ -0,0 +1,15 @@
+class Antigravity < Formula
+  desc "The ultimate VS Code God-Mode Environment Manager CLI"
+  homepage "https://github.com/Balajitechlabs/antigravity-cli"
+  url "https://github.com/Balajitechlabs/antigravity-cli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "08661de6cf2f043edd34184ea9cf52fb8f3c1077ab12f477a5f1ac0873c601e5"
+  license "MIT"
+
+  def install
+    bin.install "antigravity.sh" => "antigravity"
+  end
+
+  test do
+    system "#{bin}/antigravity", "help"
+  end
+end
